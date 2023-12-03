fee184fa78b708eacb42d2a201a7e74582802657
Alireza Bitarafan <alirezabitarafan73@yahoo.com>
2023-12-03 17:44:46 +0100

    commented by chatGpt

--- day3/part1_optimized.py
+++ day3/part1_optimized.py
@@ -7,16 +7,35 @@ What is the sum of all of the part numbers in the engine schematic?
 import re
 from collections import defaultdict
 
+# Open the file containing engine parts information
 with open("day3/engine_parts.txt", 'r') as f:
+    # Read lines from the file and split them
     lines = f.read().splitlines()
-    ans = 0
+    
+    ans = 0  # Initialize the sum of numeric values
+
+    # Iterate over each line in the grid
     for i, line in enumerate(lines):
+        # Iterate over numeric values found in the line using regex
         for m in re.finditer(r'\d+', line):
-            idx = [(i, m.start()-1), (i, m.end())]
-            idx += [(i-1, j) for j in range(m.start()-1, m.end()+1)]
-            idx += [(i+1, j) for j in range(m.start()-1, m.end()+1)]
-            # find the adjacents symbols to each number
-            count = sum(0 <= row < len(lines) and 0 <= col < len(lines[row]) and lines[row][col] != '.' and not lines[row][col].isdigit() for row, col in idx) 
+            # Create a list of positions surrounding the current numeric value
+            idx = [
+                (i, m.start()-1),
+                (i, m.end()),
+                *[(i-1, j) for j in range(m.start()-1, m.end()+1)],
+                *[(i+1, j) for j in range(m.start()-1, m.end()+1)]
+            ]
+
+            # Count the number of adjacent symbols to the current numeric value
+            count = sum(
+                0 <= row < len(lines) and 0 <= col < len(lines[row]) and
+                lines[row][col] != '.' and not lines[row][col].isdigit()
+                for row, col in idx
+            )
+
+            # If there are adjacent symbols, add the numeric value to the sum
             if count > 0:
                 ans += int(m.group())
+
+    # Print the final result
     print(ans)
--- day3/part2_optimized.py
+++ day3/part2_optimized.py
@@ -8,18 +8,33 @@ What is the sum of all of the gear ratios in your engine schematic?
 import re
 from collections import defaultdict
 
+# Open the file containing engine parts information
 with open("day3/engine_parts.txt", 'r') as f:
+    # Read lines from the file and split them
     lines = f.read().splitlines()
-    ans = 0
-    adj = defaultdict(list)
+
+    ans = 0  # Initialize the final result
+    adj = defaultdict(list)  # Default dictionary to store adjacent numeric values for each symbol
+
+    # Iterate over each line in the grid
     for i, line in enumerate(lines):
+        # Iterate over numeric values found in the line using regex
         for m in re.finditer(r'\d+', line):
-            idx = [(i, m.start()-1), (i, m.end())]
-            idx += [(i-1, j) for j in range(m.start()-1, m.end()+1)]
-            idx += [(i+1, j) for j in range(m.start()-1, m.end()+1)]
-            # find the adjacents symbols to each number
+            # Create a list of positions surrounding the current numeric value
+            idx = [
+                (i, m.start()-1),
+                (i, m.end()),
+                *[(i-1, j) for j in range(m.start()-1, m.end()+1)],
+                *[(i+1, j) for j in range(m.start()-1, m.end()+1)]
+            ]
+
+            # Find adjacent symbols to each numeric value and store them in the defaultdict
             for row, col in idx:
                 if 0 <= row < len(lines) and 0 <= col < len(lines[row]) and lines[row][col] != '.' and not lines[row][col].isdigit():
                     adj[row, col].append(int(m.group()))
-    ans = sum(a[0]*a[1] for a in adj.values() if len(a) == 2)
+
+    # Calculate the final result by summing the product of pairs of numeric values for each symbol
+    ans = sum(a[0] * a[1] for a in adj.values() if len(a) == 2)
+
+    # Print the final result
     print(ans)
